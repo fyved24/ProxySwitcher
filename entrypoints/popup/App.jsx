06d8c07cf1559b15aebfdeb71cc6d5b9d6ab5722
fyved24 <fyved24@163.com>
2024-06-03 17:23:33 +0800

    添加后端设置页面

--- entrypoints/popup/App.jsx
+++ entrypoints/popup/App.jsx
@@ -5,12 +5,19 @@ import { SwapOutlined,
   PlusOutlined,
   ArrowRightOutlined,
   ArrowUpOutlined,
-  SettingOutlined} from '@ant-design/icons';
+  SettingOutlined,
+  CheckOutlined
+} from '@ant-design/icons';
 import { Input,Button,Space } from 'antd';
 
 function App() {
   const [showAddToProxy, setShowAddToProxy] = useState(false);
   const [urlValue, setURLValue] = useState('*.example.com');
+  const [showBackendSetting, setShowBackendSetting] = useState(false);
+  const [backendURLValue, setbackendURLValue] = useState('http://127.0.0.1:8000');
+
+
+
   useEffect(() => {
     fillUrlInput();
   });
@@ -47,21 +54,24 @@ function App() {
       return null;
     }
   }
-  function handleAddClick() {
-    setShowAddToProxy(!showAddToProxy);
-  }
-
   async function submitClicked() {
     console.log(333)
     const res = await browser.runtime.sendMessage('ping');
     console.log(res); // "pong"
     
   }
+  async function settingBackendClicked() {
+    console.log(333)
+    const res = await browser.runtime.sendMessage('ping');
+    console.log(res); // "pong"
+    
+  }
+
   return (
     <>
       <ListItem ItemIco={SwapOutlined} text={'直连'}/>
       <ListItem ItemIco={ArrowRightOutlined} text={'Proxy0'}/>
-      <ListItem ItemIco={PlusOutlined} onClick={handleAddClick} text={'添加代理'} />
+      <ListItem ItemIco={PlusOutlined} onClick={()=>{setShowAddToProxy(!showAddToProxy)}} text={'添加代理'} />
       {showAddToProxy && 
       <Space.Compact
         style={{
@@ -71,10 +81,22 @@ function App() {
         <Input defaultValue='*.example.com' value={urlValue} />
         <Button type="primary" style={{
           width: '20%',
-        }} icon={<ArrowUpOutlined />} onClick={submitClicked} /> 
+        }} icon={<PlusOutlined />} onClick={submitClicked} /> 
       </Space.Compact>
       }
-      <ListItem ItemIco={SettingOutlined} text={'后端设置'}/>
+      <ListItem ItemIco={SettingOutlined} text={'后端设置'} onClick={()=>{setShowBackendSetting(!showBackendSetting)}}/>
+      {showBackendSetting && 
+      <Space.Compact
+        style={{
+          width: '100%',
+        }}
+      >
+        <Input defaultValue='http://127.0.0.1:8000' value={backendURLValue} />
+        <Button type="primary" style={{
+          width: '20%',
+        }} icon={ <CheckOutlined />} onClick={settingBackendClicked} /> 
+      </Space.Compact>
+      }
     </>
   );
 }
